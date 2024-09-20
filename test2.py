0f2ee78e2a858a1958bb7b20d789ef1b8cfb6a68
Skipper-exe <braun5538@gmail.com>
2024-09-21 00:05:51 +0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+name = "Danil"
+print(name)
+print(type(name))
+year = 24.5
+print("ТЕБЕ".upper())
+print(type(year))
+name = "Anton"
+print(name)
+name2 = ["Danil", "Anton", "Seledka", "Vodka"]
+print(name2[1: ])
+print(type(name2))
+print(len(name2))
